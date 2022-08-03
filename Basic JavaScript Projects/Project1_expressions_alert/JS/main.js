68b8a760c77b189920e338ce6855b35019a8ec2e
jefflicano82 <109245242+jefflicano82@users.noreply.github.com>
2022-08-03 11:20:19 -0700

    First Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+var B="This is a letter."
+window.alert(B)//Am writing out the var "This is a letter"//
+document.write ("Don\'t go looking for trouble <br>"
++"because those who look usually find.")
+var D="2"+"4"
+document.write(D);//am writing out the number "24"//
+var baseball= "Dodgers", Pitcher="Kershaw", Catcher="Will Smith", OF="Mookie Betts";
+document.write(Catcher);//the page should display "Will Smith"//
+document.write(10*20);//added and expression//
+var Z="This is the beginning of a string"
+var X="and this is the end of a string."
+document.write(Z+X);
