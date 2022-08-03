2bec1921045c692a8ab6f8ce241c78f3be742de5
jefflicano82 <109245242+jefflicano82@users.noreply.github.com>
2022-08-03 13:43:43 -0700

    Assignment 2

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function My_First_Function () {
+    var A= "This is the first line";
+    var B="This is the second line";
+document.getElementById("Main_Text").innerHTML=A,B;
+}
+function myFunction () {
+    var sentence="I am learning";
+    sentence+="A lot from this course!";
+    document.getElementById("Concatenate").innerHTML=sentence;
+} //this function adds this combined sentence to the html. 
