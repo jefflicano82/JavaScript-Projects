fb594601ab857ffdcce1dd1ba910f03523857400
jefflicano82 <109245242+jefflicano82@users.noreply.github.com>
2022-08-03 16:09:08 -0700

    Question

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -3,8 +3,14 @@ function My_First_Function () {
     var B="This is the second line";
 document.getElementById("Main_Text").innerHTML=A,B;
 }
+
+function Func2(p1, p2) {
+    return p1*p2;
+    document.getElementById("Func2").innerHTML=sentence;   // The function returns the product of p1 and p2
+  }
+
 function myFunction () {
     var sentence="I am learning";
     sentence+="A lot from this course!";
     document.getElementById("Concatenate").innerHTML=sentence;
-} //this function adds this combined sentence to the html. 
+}
