c6e065a40232ef901e0bacda8105fb3e65f147da
jefflicano82 <109245242+jefflicano82@users.noreply.github.com>
2022-08-05 11:06:18 -0700

    Project 5

--- Basic JavaScript Projects/Project5_Comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_Comparisons_type_coercion/JS/main.js
@@ -1,1 +1,29 @@
-document.write(typeof 3)
+document.write(typeof 3);
+document.write("10"+5);
+document.write(-3E310)
+document.write(3E310)
+document.write(10>2);
+console.log(2+2);
+console.log(13>100);
+document.write(5==5);
+document.write(5==7);
+A="Dodgers";
+B="Dodgers";
+document.write(A===B);
+C=82;
+D="82"
+document.write(C===D);
+E=82
+F="Blame it on the Boogie"
+document.write(E===F)
+G=82
+H=83
+document.write(G===H)
+document.write(10>3||3<10)
+document.write(10>11||3>10)
+function not_Function() {
+    document.getElementById("Not").innerHTML=!(5>10);
+}
+function not_Function() {
+    document.getElementById("Not").innerHTML=!(5<10);
+}
