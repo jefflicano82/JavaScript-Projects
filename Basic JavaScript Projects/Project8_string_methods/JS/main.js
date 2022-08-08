030b4a270a37a32e45de0bed660355679713791e
jefflicano82 <109245242+jefflicano82@users.noreply.github.com>
2022-08-08 14:41:01 -0700

    Project 8

---
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -0,0 +1,31 @@
+function full_Sentence() {
+  var part_1 = "I have";
+  var part_2 = "found";
+  var part_3 = "A good wife.";
+  var whole_sentence = part_1.concat(part_2, part_3);
+  document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
+let T = "Hello World!";
+let result = T.toUpperCase();
+
+let text = "Mr. the Green Knight is a headless rider";
+let position = text.search("Green");
+
+function string_Method() {
+  var X = 190;
+  document.getElementById("Numbers_to_string").innerHTML = X.toString();
+}
+
+function precision_Method() {
+  var U = 12938.30129874452;
+  document.getElementById("Precision").innerHTML = U.toPrecision(10);
+}
+
+Fire_function();
+let C = "Hello World!";
+let resulting = text;
+
+let num = 5.56789;
+console.log("let num");
+let n = num.toFixed(2);
+console.log;
